3424c7c4d7d5010859371169b8220d51ce6e5f48
Guenther Schmidt <schmig68@gmail.com>
2022-09-12 21:01:07 +0100

    initial commit

---
+++ lib/parser_builder.ex
@@ -0,0 +1,18 @@
+defmodule ParserBuilder do
+  @moduledoc """
+  Documentation for `ParserBuilder`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ParserBuilder.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/parser_builder_test.exs
@@ -0,0 +1,8 @@
+defmodule ParserBuilderTest do
+  use ExUnit.Case
+  doctest ParserBuilder
+
+  test "greets the world" do
+    assert ParserBuilder.hello() == :world
+  end
+end
